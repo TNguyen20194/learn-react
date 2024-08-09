88070b3a33929873ea696857dfe816821735cad1
Truong Nguyen <truong.nguyen201@gmail.com>
2024-08-08 23:02:42 -0400

    Added componentDidMount() method to update state after fetching data from an API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import React, { Component } from 'react'
+import CardList from './CardList'
+import SearchBox from './SearchBox'
 // non-default export of object from robot.js => use destructuring syntax
-import { robots } from './robots';
-import './App.css';
+import './App.css'
 
 class App extends Component {
   constructor () {
@@ -11,31 +10,42 @@ class App extends Component {
     //the state is what changes in the App & describes the App
     // React uses this state to render the App - passes the state down as props to the components
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
+  // Updating state after fetching data from an API
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users })
+      )
+  }
 
-  onSearchChange = (event) => {
+  onSearchChange = event => {
     // Update state.searchfield value using setState()
-    this.setState({searchfield: event.target.value})
+    this.setState({ searchfield: event.target.value })
   }
 
   render () {
     // Filter robots state to now what includes in the searchfield
     const filteredRobots = this.state.robots.filter(robots => {
-        return robots.name
-          .toLowerCase()
-          .includes(this.state.searchfield.toLowerCase())
-      })
-    return (
-      <div className='tc'>
-        <h1 className='f1'>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase())
+    })
+    if( this.state.robots.length === 0) {
+        return <h1 className='tc'>Loading...</h1>
+    } else {
+        return (
+            <div className='tc'>
+              <h1 className='f1'>Robofriends</h1>
+              <SearchBox searchChange={this.onSearchChange} />
+              <CardList robots={filteredRobots} />
+            </div>
+          )
+    }
   }
 }
 
